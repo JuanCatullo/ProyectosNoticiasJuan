e7bbdbd2a40dbb76dec0ee8d067c11828f4330c4
JuanCatullo <juancatullo99@gmail.com>
2023-03-20 18:05:12 -0300

    Insert, modify, delete users

--- Manager/Usuario.aspx.cs
+++ Manager/Usuario.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +15,132 @@ namespace ProyectosNoticiasJuan.Manager
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public static string EliminarUsuario(int iId)
+        {
+            SqlConnection MyConnection = default(SqlConnection);
+            SqlCommand MyCommand = default(SqlCommand);
+
+            try
+            {
+                MyConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringSQL"].ConnectionString);
+                MyCommand = new SqlCommand("spEliminarUsuario", MyConnection);
+                MyCommand.CommandType = CommandType.StoredProcedure;
+
+
+                MyCommand.Parameters.AddWithValue("@id", iId);
+
+                //ACCIONES
+                MyConnection.Open();
+                MyCommand.ExecuteNonQuery();
+                MyConnection.Close();   //abrir y cerrar es critico a nivel performance
+                MyConnection.Dispose();
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+
+
+        }
+
+        public static string ActualizarUsuario(int iId, string txtname, string txtlastname, string txtdni, int ddlpais, int miRadioButtonList, string email, string Fechanac, string otros, string password)
+        {
+            SqlConnection MyConnection = default(SqlConnection);
+            SqlCommand MyCommand = default(SqlCommand);
+
+            try
+            {
+                MyConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringSQL"].ConnectionString);
+                MyCommand = new SqlCommand("spActualizarUsuario", MyConnection);
+                MyCommand.CommandType = CommandType.StoredProcedure;
+
+                //AGREGO EL PARAMETRO CON EL VALOR DEL PARAMETRO AL COMANDO
+                MyCommand.Parameters.AddWithValue("@id", iId);
+
+                MyCommand.Parameters.AddWithValue("@nombre", txtname);
+
+                MyCommand.Parameters.AddWithValue("@apellido", txtlastname);
+
+                MyCommand.Parameters.AddWithValue("@dni", txtdni);
+
+                MyCommand.Parameters.AddWithValue("@id_pais", ddlpais);
+
+                MyCommand.Parameters.AddWithValue("@curso_id", miRadioButtonList);
+
+                MyCommand.Parameters.AddWithValue("@email", email);
+
+                MyCommand.Parameters.AddWithValue("@fecha_nacimiento", Fechanac);
+
+                MyCommand.Parameters.AddWithValue("@conocimientos", otros);
+
+                MyCommand.Parameters.AddWithValue("@contraseña", password);
+
+                //ACCIONES A MANO
+                MyConnection.Open(); //ABRO CONEXION
+                MyCommand.ExecuteNonQuery(); //EJECUTO COMANDO
+                MyConnection.Close(); //CIERRO CONEXION
+                MyConnection.Dispose(); //DESCARTO CONEXION
+
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public static string InsertarUsuario(int iId, string txtname, string txtlastname, string txtdni, int ddlpais, int miRadioButtonList, string email, string Fechanac, string otros, string password)
+        {
+            SqlConnection MyConnection = default(SqlConnection);
+            SqlCommand MyCommand = default(SqlCommand);
+
+            try
+            {
+                MyConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringSQL"].ConnectionString);
+                MyCommand = new SqlCommand("spInsertarUsuario", MyConnection);
+                MyCommand.CommandType = CommandType.StoredProcedure;
+
+                //AGREGO EL PARAMETRO CON EL VALOR DEL PARAMETRO AL COMANDO
+
+                MyCommand.Parameters.AddWithValue("@nombre", txtname);
+
+                MyCommand.Parameters.AddWithValue("@apellido", txtlastname);
+
+                MyCommand.Parameters.AddWithValue("@dni", txtdni);
+
+                MyCommand.Parameters.AddWithValue("@id_pais", ddlpais);
+
+                MyCommand.Parameters.AddWithValue("@curso_id", miRadioButtonList);
+
+                MyCommand.Parameters.AddWithValue("@email", email);
+
+                MyCommand.Parameters.AddWithValue("@fecha_nacimiento", Fechanac);
+
+                MyCommand.Parameters.AddWithValue("@conocimientos", otros);
+
+                MyCommand.Parameters.AddWithValue("@contraseña", password);
+
+
+
+
+
+
+                //ACCIONES A MANO
+                MyConnection.Open(); //ABRO CONEXION
+                MyCommand.ExecuteNonQuery(); //EJECUTO COMANDO
+                MyConnection.Close(); //CIERRO CONEXION
+                MyConnection.Dispose(); //DESCARTO CONEXION
+
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
     }
 }
--- Utilidades/Utils.cs
+++ Utilidades/Utils.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 
-namespace ProyectoJuan.Utilidades
+namespace ProyectoNoticiasJuan.Utilidades
 {
     public class Utils
     {
